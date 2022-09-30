c80cd21f3e75e03eeb1507b95bd3ad58ab52614d
sol <guoshengyang97@penguin>
2022-09-30 19:38:21 +0800

    auto add

--- js/actionsome.js
+++ js/actionsome.js
@@ -1,4 +1,5 @@
 
+
 function lineMove() {
     var elem = document.getElementById("line");   
     var pos = 0;
@@ -10,26 +11,47 @@ function lineMove() {
     }
     function frame() {
         if(pos==100){
-            cline1Move("cline1");
+            cline1Move("cline1",pos);
             displayhtml("html1")
           } 
           if(pos==200){
-            cline2Move();
+            cline2Move("cline2",pos);
             displayhtml("html2")
           }
-      if (pos == 350) {
+          if(pos==300){
+            addcline("line","cline3");
+            clinedit("cline3","shanshan.html","shanshan");
+            cline1Move("cline3",pos);
+          }
+          if(pos==400){
+            addcline("line","cline4");
+            clinedit("cline4","midou.html","midou");
+            cline2Move("cline4",pos);
+          }
+          if(pos%100==0 && pos>400){
+            clineid = "cline"+pos/100;
+            addcline("line",clineid);
+            if(pos/100%2==0){
+              cline2Move(clineid,pos);
+            }else{
+              cline1Move(clineid,pos);
+
+            }
+          }
+      if (pos == 450) {
         clearInterval(id);
       } 
       else {
         stepadd();
+        movehtml(pos);
       }
     }
   }
 
-  function cline1Move( id) {
+  function cline1Move( id,tpos) {
     var elem = document.getElementById(id);  
     var pos = 0;
-    elem.style.top = "100px";
+    elem.style.top = tpos+ "px";
     var id = setInterval(frame1, 6);
     function frame1() {
       if (pos == 200) {
@@ -41,11 +63,12 @@ function lineMove() {
     }
   }
 
-  function cline2Move() {
-    var elem = document.getElementById("cline2");   
+  function cline2Move(id,tpos) {
+    var elem = document.getElementById(id);   
     var pos = 0;
     var left=0;
-    elem.style.top = "200px";
+    elem.style.top = tpos+ "px";
+    elem.style.backgroundColor = "purple";
     var id = setInterval(frame2, 6);
     function frame2() {
       if (pos == 200) {
@@ -54,7 +77,8 @@ function lineMove() {
         pos++; 
         left--;
         elem.style.width = pos+"px"; 
-        elem.style.left = left+"px"
+        elem.style.left = left+"px";
+
       }
     }
   }
@@ -62,6 +86,24 @@ function lineMove() {
     var elem = document.getElementById(id);   
     elem.style.display = "block"
   }
-  
+  function addcline(id,childid){
+  var elem = document.getElementById(id);
+  var newdiv = document.createElement("div");
+  newdiv.setAttribute("class","cline");
+  newdiv.setAttribute("id",childid);
+  elem.appendChild(newdiv);
+  }
+  function movehtml(tpos){
+   window.scrollBy(0,tpos);
+  }
+function clinedit(id,url,name){
+  var cline =document.getElementById(id);
+  var tempp = document.createElement("p");
+  var aurl = document.createElement("a");
+  aurl.setAttribute("href",url);
+  aurl.textContent=name;
+  tempp.appendChild(aurl);
+  cline.appendChild(tempp);
+}
 setTimeout(1000);
 lineMove();
